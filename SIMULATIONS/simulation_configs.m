86c23ca82cac366cc4cfec85c46683751a85e567
SRSteinkamp <simon.steinkamp@googlemail.com>
2023-11-13 10:54:21 +0100

    adding empty simulation configs

---
+++ SIMULATIONS/simulation_configs.m
@@ -0,0 +1,22 @@
+function [configs] = simulation_configs()
+    % Simulation configs, file for handling of paths to toolboxes.
+    % ---------------------------------------------------------------------
+    % Copyright (C) 2023 Simon R. Steinkamp, Iyadh Chaker
+    % This program is free software: you can redistribute it and/or modify
+    % it under the terms of the GNU General Public License as published by
+    % the Free Software Foundation, either version 3 of the License, or
+    % (at your option) any later version.
+    %
+    % This program is distributed in the hope that it will be useful,
+    % but WITHOUT ANY WARRANTY; without even the implied warranty of
+    % MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    % GNU General Public License for more details.
+    %
+    % You should have received a copy of the GNU General Public License
+    % along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    % ---------------------------------------------------------------------
+
+    configs.spm_path = ''; % Path to spm toolbox https://github.com/spm/spm12
+    configs.vba_path = ''; % Path to VBA toolbox https://github.com/MBB-team/VBA-toolbox
+
+end
